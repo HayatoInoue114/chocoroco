1823ef34bc7eae5dc078e324de5aada117d59951
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2025-05-16 16:58:54 +0900

    a

---
+++ Assets/scripts/TaskManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class TaskManager : MonoBehaviour
+{
+    enum Task
+    {
+        taskType,
+
+    }
+
+    //クリアしたタスクの数
+    public static int clearedTaskCount = 0;
+    int nowTask = 1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
